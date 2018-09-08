aebb49416013079224a2342547bf81bf04c453d0
aminkhavariionic <43031205+aminkhavariionic@users.noreply.github.com>
2018-09-08 22:33:16 +0430

    0

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 angular.module("woocommerce-api").constant("CONFIG", {
 
     // The url of your domain, both HTTP and HTTPS are supported.
-    site_url: 'http://mobile-apps.today/products/ionic/woocommerce-api',
+    site_url: 'http://androidsrc.ir',
     // site_url: 'http://localhost:8888/mobile/woocommerce-api/wordpress',
 
     // Max period of time to wait for reply from the server, defined in milliseconds.
@@ -16,8 +16,8 @@ angular.module("woocommerce-api").constant("CONFIG", {
 
     // Pair of credentials from your woocommerce installation, please refer to the documentation.
     // Apps today
-    wc_consumer_key: 'ck_5f886e3d5c14cb0280167e5c96615e59a07b2124',
-    wc_consumer_secret: 'cs_4a5d2c133acc4203d45b410cf830f2755bf3257e',
+    wc_consumer_key: 'ck_5ce86be0e8a9119073f85f00a7dc65abf1ba4390',
+    wc_consumer_secret: 'cs_dbba4eeffdffffd5e78e34e8e7cf465683b37665',
     // Localhost
     // wc_consumer_key: 'ck_86a13f2c0ec34a4c99374e120dba436d31f069c3',
     // wc_consumer_secret: 'cs_a0bc69e05eced4a8f464c08849c8f543946448d9',
